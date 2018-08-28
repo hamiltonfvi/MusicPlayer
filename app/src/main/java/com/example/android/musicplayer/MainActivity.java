74bc3483c2ed63e1695e5781a7d9331b8e32ccfb
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-08-27 23:07:57 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button playbutton, pausebutton;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initialize views
-    playbutton = findViewById(R.id.play_button);
-    pausebutton = findViewById(R.id.pause_button);
-    }
+        playbutton = findViewById(R.id.play_button);
+        pausebutton = findViewById(R.id.pause_button);
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        pausebutton.setEnabled(false);
 
-    //play or pause song when clicked
-
-    public void play(View view) {
+    //play song when clicked
         playbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final MediaPlayer ring = MediaPlayer.create(this,R.raw.song);
+                Toast.makeText(getApplicationContext(), getString(R.string.playing),Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                pausebutton.setEnabled(true);
+                playbutton.setEnabled(false);
+            }
+        });
+
+    //pause song when clicked
+        pausebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), getString(R.string.pausing),Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+                pausebutton.setEnabled(false);
+                playbutton.setEnabled(true);
             }
         });
     }
-
 }
