e20cbf02e9ca569c84c28f8806731a80b9fff88a
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-08-31 16:17:56 -0400

    -added a call back function that notifies the user the reproduction of the audio music has finished. -added disable play and pause buttons during certain times of reproduction. -changed the audio file per a smaller one -added a personalized icon

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.media.MediaPlayer.OnCompletionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,26 +22,56 @@ public class MainActivity extends AppCompatActivity {
         playbutton = findViewById(R.id.play_button);
         pausebutton = findViewById(R.id.pause_button);
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
+
+        //Pause button gotta start disable by default
         pausebutton.setEnabled(false);
 
-    //play song when clicked
+        //play song when play button is clicked
         playbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), getString(R.string.playing),Toast.LENGTH_SHORT).show();
+
+                //Display a Toast message
+                Toast.makeText(getApplicationContext(), getString(R.string.playing), Toast.LENGTH_SHORT).show();
+
+                //Start Playing the audio file
                 mediaPlayer.start();
+
+                //Pop up a message when the reproduction of the music has finished
+                mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.ended), Toast.LENGTH_SHORT).show();
+
+                        //Disable Pause Button after reproduction of the audio is finished
+                        pausebutton.setEnabled(false);
+
+                        //Enable Play Button after reproduction of the audio is finished
+                        playbutton.setEnabled(true);
+                    }
+                });
+
+                //Enable Pause Button while playing the audio file
                 pausebutton.setEnabled(true);
+
+                //Disable Play Button until media player has finished reproducing
                 playbutton.setEnabled(false);
             }
         });
 
-    //pause song when clicked
+        //pause song when pause button is clicked
         pausebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), getString(R.string.pausing),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), getString(R.string.pausing), Toast.LENGTH_SHORT).show();
+
+                //Pause the reproduction of the audio file
                 mediaPlayer.pause();
+
+                //Disable Pause Button after clicked
                 pausebutton.setEnabled(false);
+
+                //Enable Play Button after unpause the audio
                 playbutton.setEnabled(true);
             }
         });
